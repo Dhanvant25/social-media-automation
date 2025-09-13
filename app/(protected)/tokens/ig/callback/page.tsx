59efaf38768231fa846edf845601ae16ac3c95f8
Dhanvant Sonagara <dhanvant.closedigit@gmail.com>
2025-09-13 11:55:39 +0530

    Commet Some Code

--- app/(protected)/tokens/ig/callback/page.tsx
+++ app/(protected)/tokens/ig/callback/page.tsx
@@ -51,31 +51,31 @@ export default function InstagramCallbackPage() {
 
       console.log("Short Token Res", shortTokenRes);
 
-      const shortLivedToken = shortTokenRes.data.access_token;
-      const userId = shortTokenRes.data.user_id;
+      // const shortLivedToken = shortTokenRes.data.access_token;
+      // const userId = shortTokenRes.data.user_id;
 
-      const longTokenRes = await axios.get(
-        `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.NEXT_PUBLIC_IG_APP_SECRET}&access_token=${shortLivedToken}`,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
+      // const longTokenRes = await axios.get(
+      //   `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.NEXT_PUBLIC_IG_APP_SECRET}&access_token=${shortLivedToken}`,
+      //   {
+      //     headers: {
+      //       "Content-Type": "application/x-www-form-urlencoded",
+      //     },
+      //   }
+      // );
 
-      const longLivedToken = longTokenRes.data.access_token;
+      // const longLivedToken = longTokenRes.data.access_token;
 
-      console.log("Long Lived Token", longLivedToken);
+      // console.log("Long Lived Token", longLivedToken);
 
-      if (longLivedToken) {
-        const platform = platforms.find(
-          (platform) => platform.key == "instagram"
-        );
-        if (platform) {
-          saveSocialMediaTokens(platform.id, longLivedToken, null, null, true);
-          router.push("/tokens");
-        }
-      }
+      // if (longLivedToken) {
+      //   const platform = platforms.find(
+      //     (platform) => platform.key == "instagram"
+      //   );
+      //   if (platform) {
+      //     saveSocialMediaTokens(platform.id, longLivedToken, null, null, true);
+      //     router.push("/tokens");
+      //   }
+      // }
     } catch (error) {
       console.log("IG Token Generate Error", error);
     }
