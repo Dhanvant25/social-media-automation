49234aa99c3efe53a9c1ee85ee693d29654e0e89
Dhanvant Sonagara <dhanvant.closedigit@gmail.com>
2025-09-13 10:59:07 +0530

    Ig Backend Code Some Changes

--- app/api/instagram/token/route.ts
+++ app/api/instagram/token/route.ts
@@ -6,11 +6,22 @@ export async function POST(req: Request) {
   try {
     const { code } = await req.json();
 
+    console.log(
+      "==========================================CALL MY IG BG API=========================================="
+    );
+
     const formData = new URLSearchParams();
-    formData.append("client_id", process.env.NEXT_PUBLIC_IG_APP_ID!);
-    formData.append("client_secret", process.env.NEXT_PUBLIC_IG_APP_SECRET!);
+    // formData.append("client_id", process.env.NEXT_PUBLIC_IG_APP_ID!);
+    // formData.append("client_secret", process.env.NEXT_PUBLIC_IG_APP_SECRET!);
+    // formData.append("grant_type", "authorization_code");
+    // formData.append("redirect_uri", process.env.NEXT_PUBLIC_IG_REDIRECT_URI!);
+    formData.append("client_id", "1129344475914140");
+    formData.append("client_secret", "6f2a10e8fc37c96f7d51bc6ac6fa7406");
     formData.append("grant_type", "authorization_code");
-    formData.append("redirect_uri", process.env.NEXT_PUBLIC_IG_REDIRECT_URI!);
+    formData.append(
+      "redirect_uri",
+      "https://social-media-automation-d9vjebdmf-dhanvant-sonagaras-projects.vercel.app/tokens/ig/callback"
+    );
     formData.append("code", code);
 
     const response = await axios.post(
